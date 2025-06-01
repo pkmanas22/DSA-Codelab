ea240c2c4c6377638c3565d5230b55cc72237495
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-06-01 19:57:34 +0530

    fix(api): cors error

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.BASE_URL,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Accept'],
